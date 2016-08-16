4e2fdc1d8d22cd4ae2836ccc858411cfcc0f4970
mixelpixel <mixelpixel@users.noreply.github.com>
2016-08-16 04:39:24 -0400

    Add files via upload

--- ch2.rb
+++ ch2.rb
@@ -3,6 +3,8 @@ http://pdf.th7.cn/down/files/1502/Practical%20Object-Oriented%20Design%20in%20Ru
 Chapter 2: Designing Classes with a Single Responsibility
 =end
 
+# a procedure for determining the gear ratio
+
 chainring = 52 #number of teeth
 cog = 11
 ratio = chainring / cog.to_f
