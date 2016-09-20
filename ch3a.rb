ddb128a2b88e21f1225e6769170d714275b3f23b
Patrick Kennedy <mixelpix@mac.com>
2016-09-20 00:48:54 -0400

    ch3

--- ch3a.rb
+++ ch3a.rb
@@ -6,7 +6,7 @@ Chapter 3. Managing Dependencies, Understanding Dependencies pg.36
 =begin
     An object depends on another object if, when one object changes, the other might be
 forced to change in turn.
-    Here’s a modified version of the Gear class, where Gear is initialized with four familiar
+    Here's a modified version of the Gear class, where Gear is initialized with four familiar
 arguments. The gear_inches method uses two of them, rim and tire, to
 create a new instance of Wheel. Wheel has not changed since you last you saw it in
 Chapter 2, Designing Classes with a Single Responsibility.
