0f464ad604f8722b78c642abddc98b09857be602
Patrick Kennedy <mixelpix@mac.com>
2016-09-21 01:17:14 -0400

    ch6 - CLASSical inheritance

---
+++ ch6q.rb
@@ -0,0 +1,129 @@
+=begin
+http://pdf.th7.cn/down/files/1502/Practical%20Object-Oriented%20Design%20in%20Ruby.pdf
+CHAPTER 6 - Acquiring Behavior Through Inheritance, Decoupling Subclasses Using Hook Messages pg. 136-138
+=end
+
+
+class Bicycle
+  attr_reader :size, :chain, :tire_size
+
+  def initialize(args={})                             # <- WHY THE HASH FOR args={} ???
+    @size      = args[:size]
+    @chain     = args[:chain]     || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+    post_initialize(args)
+  end
+
+  def spares
+    { tire_size:  tire_size,
+      chain:      chain}.merge(local_spares)
+  end
+
+  def default_tire_size
+    raise NotImplementedError,
+          "This #{self.class} cannot respond to: "
+  end
+
+  # subclasses may override
+  def post_initialize(args)
+    nil
+  end
+
+  def local_spares
+    {}
+  end
+
+  def default_chain
+    '10-speed'
+  end
+end
+
+
+
+class RoadBike < Bicycle
+  attr_reader :tape_color
+
+  def post_initialize(args)
+    @tape_color = args[:tape_color]
+  end
+
+  def local_spares
+    {tape_color: tape_color}
+  end
+
+  def default_tire_size
+    '23'
+  end
+end
+
+
+
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def post_initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
+  end
+
+  def local_spares
+    {rear_shock: rear_shock}
+  end
+
+  def default_tire_size
+    '2.1'
+  end
+end
+
+
+
+class RecumbentBike < Bicycle
+  attr_reader :flag
+
+  def post_initialize(args)
+    @flag = args[:flag]
+  end
+
+  def local_spares
+    {flag: flag}
+  end
+
+  def default_chain
+    '9-speed'
+  end
+
+  def default_tire_size
+    '28'
+  end
+end
+
+
+
+bent = RecumbentBike.new(flag: 'tall and orange')
+puts bent.spares.to_s.split(', ')
+# -> {:tire_sicze => "28",
+#     :chain     => "9-speed",
+#     :flag      => 'tall and orange'}
+puts
+
+road_bike = RoadBike.new(
+              size:        'M',
+              tape_color:  'red' )
+
+puts road_bike.spares.to_s.split(', ')
+puts
+
+mountain_bike = MountainBike.new(
+                  size:         'S',
+                  front_shock:  'Manitou',
+                  rear_shock:   'Fox')
+
+puts mountain_bike.spares.to_s.split(', ')
+puts
+
+puts road_bike.inspect.split(' ')
+puts
+puts mountain_bike.inspect.split(' ')
+puts
+puts bent.inspect.split(' ')
+puts
